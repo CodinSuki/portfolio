02f86295ebcee5cd415fd7aea6354498a6aa2821
Sky <122413784+CodinSuki@users.noreply.github.com>
2025-11-26 13:27:38 +0800

    Fix toggleButton declaration and add testbutton

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const body = document.body;
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
+const testbutton = document.getElementById('theme-test');
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+});

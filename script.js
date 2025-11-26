2392bb16973b3a859b660e024e73b26e4106fe73
Sky <122413784+CodinSuki@users.noreply.github.com>
2025-11-26 13:34:00 +0800

    Fix testbutton declaration and event listener

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const toggleButton = document.getElementById('theme-toggle');
-const testbutton = document.getElementById('theme-test');
+
 const body = document.body;
 
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
-
+const testbutton = document.getElementById('theme-test');
 testbutton.addEventListener('click', () => {
     window.alert("I don't have a formal photo sorry sir D:");
 });

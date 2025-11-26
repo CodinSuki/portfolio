a8ac57a8c61667350f73037ded2ffaa97d752bda
Sky <122413784+CodinSuki@users.noreply.github.com>
2025-11-26 13:34:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const toggleButton = document.getElementById('theme-toggle');
-
 const body = document.body;
 
 
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
-const testbutton = document.getElementById('theme-test');
-testbutton.addEventListener('click', () => {
+
+
+const testButton = document.getElementById('theme-test');
+testButton.addEventListener('click', () => {
     window.alert("I don't have a formal photo sorry sir D:");
 });

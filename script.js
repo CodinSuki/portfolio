589ab096bbb1a998482fd81fcfe00fd21097b239
Sky <122413784+CodinSuki@users.noreply.github.com>
2025-11-26 13:33:33 +0800

    Fix duplicate declaration of testbutton

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const toggleButton = document.getElementById('theme-toggle');
+const testbutton = document.getElementById('theme-test');
 const body = document.body;
+
+
 toggleButton.addEventListener('click', () => {
     body.classList.toggle('dark');
 });
-const testbutton = document.getElementById('theme-test');
-toggleButton.addEventListener('click', () => {
+
+testbutton.addEventListener('click', () => {
     window.alert("I don't have a formal photo sorry sir D:");
 });

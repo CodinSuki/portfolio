5d359e49e056841d0638e416ac5c088d2213ad8f
Sky <122413784+CodinSuki@users.noreply.github.com>
2025-12-10 11:15:44 +0800

    changes to html js and css structure layout
    changes to html js and css structure layout

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.body;
+    const slider = document.querySelector('.feature-carousel');
+    const container = slider.querySelector('.feature-container');
+    const toggleButton = document.getElementById('theme-toggle');
+    const testButton = document.getElementById('theme-test');
 
+    // CenterCarousel
+    slider.scrollLeft = (container.scrollWidth - slider.clientWidth) / 2;
 
-toggleButton.addEventListener('click', () => {
-    body.classList.toggle('dark');
-});
+    // CarouselDrag
+    let isDown = false;
+    let startX;
+    let scrollLeft;
 
+    slider.addEventListener('mousedown', (e) => {
+        isDown = true;
+        slider.classList.add('active');
+        startX = e.pageX - slider.offsetLeft;
+        scrollLeft = slider.scrollLeft;
+    });
 
-const testButton = document.getElementById('theme-test');
-testButton.addEventListener('click', () => {
-    window.alert("I don't have a formal photo sorry sir D:");
+    slider.addEventListener('mouseleave', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mouseup', () => {
+        isDown = false;
+        slider.classList.remove('active');
+    });
+
+    slider.addEventListener('mousemove', (e) => {
+        if (!isDown) return;
+        e.preventDefault();
+        const x = e.pageX - slider.offsetLeft;
+        const walk = (x - startX) * 2; // Adjust scroll speed
+        slider.scrollLeft = scrollLeft - walk;
+    });
+
+    // dark mode
+    toggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark');
+    });
+
+    /*     
+        testButton.addEventListener('click', () => {
+            window.alert("I don't have a formal photo sorry sir D:");
+        });
+    }
+        */
 });
